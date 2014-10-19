7873e7b4059605aa5a9d53d09b09c01959466770
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-10-19 15:15:48 -0400

    Add accordian

---
+++ source/javascripts/accordian.js
@@ -0,0 +1,9 @@
+//= require jquery.min
+
+$(function(){
+  $('.js-accordion-trigger').bind('click', function(e){
+    jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
+    jQuery(this).parent().toggleClass('is-expanded');
+    e.preventDefault();
+  });
+});
