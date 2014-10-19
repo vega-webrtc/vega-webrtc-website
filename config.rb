21b8018b8a21c3100fe60049d3d29bb1f77a4b2e
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-10-19 15:04:54 -0400

    create layout for the guides

--- config.rb
+++ config.rb
@@ -28,6 +28,8 @@
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+page "guides/*", layout: :guides
+
 ###
 # Helpers
 ###
